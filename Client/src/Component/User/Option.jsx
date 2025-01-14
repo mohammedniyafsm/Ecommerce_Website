2876c2f0ac724ddbab49b4539fda440f284af2e5
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-01-14 21:06:08 +0530

    change naviagtion

--- Client/src/Component/User/Option.jsx
+++ Client/src/Component/User/Option.jsx
@@ -7,7 +7,7 @@ function Option() {
   const Navigate=useNavigate();
   return (
     <div className='w-screen h-96 flex items-center justify-center gap-12 px-44 pt-20'>
-      <div className="relative flex w-[365px] h-60 border border-gray-200  cursor-pointer overflow-hidden group" onClick={()=>Navigate('/product')}>
+      <div className="relative flex w-[365px] h-60 border border-gray-200  cursor-pointer overflow-hidden group" onClick={()=>Navigate('/shop')}>
         <div className='pt-8 pl-10 z-10'>
           <h1 className='text-3xl font-bold text-gray-800 group-hover:text-white'>Women</h1>
           <h2 className='text-sm font-medium text-gray-500 mt-2 group-hover:text-white'>Spring 2025</h2>
@@ -18,7 +18,7 @@ function Option() {
           <img className='h-full w-full object-cover object-center' src={pic.women1} alt="Women" />
         </div>
       </div>
-      <div className="relative flex w-[365px] h-60 border border-gray-200  cursor-pointer overflow-hidden group" onClick={()=>Navigate('/product')}>
+      <div className="relative flex w-[365px] h-60 border border-gray-200  cursor-pointer overflow-hidden group" onClick={()=>Navigate('/shop')}>
         <div className='pt-8 pl-10 z-10'>
           <h1 className='text-3xl font-bold text-gray-800 group-hover:text-white'>Men</h1>
           <h2 className='text-sm font-medium text-gray-500 mt-2 group-hover:text-white'>Spring 2025</h2>
@@ -29,7 +29,7 @@ function Option() {
           <img className='h-full w-full object-cover object-center' src={pic.men1} alt="Men" />
         </div>
       </div>
-      <div className="relative flex w-[365px] h-60 border border-gray-200  cursor-pointer overflow-hidden group" onClick={()=>Navigate('/product')}>
+      <div className="relative flex w-[365px] h-60 border border-gray-200  cursor-pointer overflow-hidden group" onClick={()=>Navigate('/shop')}>
         <div className='pt-8 pl-10 z-10'>
           <h1 className='text-3xl font-bold text-gray-800 group-hover:text-white'>Accessories</h1>
           <h2 className='text-sm font-medium text-gray-500 mt-2 group-hover:text-white'>New Trend</h2>
