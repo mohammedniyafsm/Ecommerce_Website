fe62b5fa27be1b509cd41dbb02ef22150ebdf1a0
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-01-19 17:51:29 +0530

    user checkout page

---
+++ Client/src/Pages/User/Checkout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../Component/User/Navbar';
+import UserCheckout from '../../Component/User/Navbar';
+
+function Checkout() {
+  return (
+    <>
+    <Navbar/>
+    <UserCheckout/>
+    </>
+  )
+}
+
+export default Checkout
