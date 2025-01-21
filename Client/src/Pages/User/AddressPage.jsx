5bd8b4d4a0e2ebb801796cbcbee39cb43b719b7c
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-01-21 09:02:28 +0530

    added address page

---
+++ Client/src/Pages/User/AddressPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../Component/User/Navbar';
+import AddAddress from '../../Component/User/AddAddress';
+
+function AddressPage() {
+  return (
+    <>
+    <Navbar/>
+    <AddAddress/>
+      
+    </>
+  )
+}
+
+export default AddressPage
